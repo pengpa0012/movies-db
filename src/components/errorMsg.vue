dc226711a87fd9aa7f9ec086930910afd58c7408
pengpa0012 <peng.ilikha@gmail.com>
2021-06-17 07:41:00 +0800

    change text on error message

--- src/components/errorMsg.vue
+++ src/components/errorMsg.vue
@@ -3,8 +3,8 @@
     <h1>Error Occured</h1>
     <p>
       Can't get the data on official YTS website. <br />
-      Click <a href="https://yts.mx/" target="_blank">here</a> to if you can
-      visit their site.
+      Click <a href="https://yts.mx/" target="_blank">here</a> to check if you
+      can visit their site.
     </p>
   </div>
 </template>
